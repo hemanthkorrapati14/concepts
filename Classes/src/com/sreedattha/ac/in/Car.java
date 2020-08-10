d3b2342364731afa42717fc50a68ed5669b160ca
hemanthkorrapati14@gmail.com <hemanthkorrapati14@gmail.com>
2020-08-10 15:25:11 +0530

    project changes for classes dir
    	Initial commit

---
+++ Classes/src/com/sreedattha/ac/in/Car.java
@@ -0,0 +1,19 @@
+package com.sreedattha.ac.in;
+
+public class Car {
+
+    private int Doors;
+    private int Wheels;
+    public String Model;
+    private String Engine;
+    private String Colour;
+
+    public void setModel(String model ){
+        this.Model=model;
+    }
+     public String getModel(){
+        return this.Model;
+     }
+
+
+}
---
+++ Classes/src/com/sreedattha/ac/in/Main.java
@@ -0,0 +1,12 @@
+package com.sreedattha.ac.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car porsche =new Car();
+        porsche.setModel("carrare");
+        System.out.println("Model is"+ porsche.getModel());
+        
+    }
+}
